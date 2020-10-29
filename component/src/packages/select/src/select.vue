594c4e79634320ad6327fb8024d768883e4efa60
study0918 <beyondthestars@yeah.net>
2020-10-29 23:35:46 +0800

    select

--- component/src/packages/select/src/select.vue
+++ component/src/packages/select/src/select.vue
@@ -1,8 +1,22 @@
 <template>
-  <div>222</div>
+  <div
+    class="el-select"
+    :class="[selectSize ? 'el-select--' + selectSize : '']"
+  ></div>
 </template>
 <script>
 export default {
-  name: "ElSelect"
+  name: "ElSelect",
+  props: {
+    size: String
+  },
+  computed: {
+    _elFormItemSize() {
+      return (this.elFormItem || {}).elFormItemSize;
+    },
+    selectSize() {
+      return this.size || this._elFormItemSize;
+    }
+  }
 };
 </script>
