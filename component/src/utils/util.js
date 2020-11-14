3e20e0b1d445ac3dfa0da1479cdc12537d5b29d7
study0918 <beyondthestars@yeah.net>
2020-11-14 12:45:08 +0800

    input

--- component/src/utils/util.js
+++ component/src/utils/util.js
@@ -1,6 +1,6 @@
-export const getValueByPath = function (object, prop) {
+export const getValueByPath = function(object, prop) {
   prop = prop || "";
-  const paths = prop.split('.');
+  const paths = prop.split(".");
   let current = null;
   let result = null;
   for (let i = 0, j = path.length; i < j; i++) {
@@ -10,7 +10,15 @@ export const getValueByPath = function (object, prop) {
       result = current[path];
       break;
     }
-    current = current[path]
+    current = current[path];
   }
   return result;
+};
+
+export const kebabCase = function(str) {
+  const hyphenateRE = /([^-])([A-Z])/g;
+  return str
+    .replace(hyphenateRE, "$1-$2")
+    .replace(hyphenateRE, "$1-$2")
+    .toLowerCase();
 };
