56be6930bd870b6c43c6915ce6ea1c674cde3a54
study0918 <beyondthestars@yeah.net>
2020-11-15 13:00:22 +0800

    input

--- component/src/examples/input.vue
+++ component/src/examples/input.vue
@@ -1,10 +1,14 @@
 <template>
   <div>
+    basic:
     <el-input
       v-model="input"
       placeholder="请输入内容"
       style="width:400px"
     ></el-input>
+    <hr />
+    clearable:
+    <el-input placeholder="请输入内容" v-model="input" style="width:400px" clearable> </el-input>
   </div>
 </template>
 <script>
