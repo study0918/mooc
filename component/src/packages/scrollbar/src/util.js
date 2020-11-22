963a158838f3782a6fd66eeba6ee17e417f897ab
study0918 <beyondthestars@yeah.net>
2020-11-22 23:23:14 +0800

    scroller

---
+++ component/src/packages/scrollbar/src/util.js
@@ -0,0 +1,12 @@
+export const BAR_MAP = {
+  vertical: {
+    offset: "offsetHeight",
+    scroll: "scrollTop",
+    scrollSize: "scrollHeight",
+    size: "height",
+    key: "vertical",
+    axis: "Y",
+    client: "clientY",
+    direction: "top"
+  }
+};
