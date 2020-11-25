8d016d51d0405fadc126365f424b014f7ff4e142
study0918 <beyondthestars@yeah.net>
2020-11-25 12:30:36 +0800

    Update main.js

--- component/src/packages/scrollbar/src/main.js
+++ component/src/packages/scrollbar/src/main.js
@@ -1,2 +1,32 @@
-import { addResizeListener, removeResizeListener } from '../../../utils/resize-event';
-import scrollbarWidth from '../../../utils/scrollbar-width'
+import {
+  addResizeListener,
+  removeResizeListener,
+} from "../../../utils/resize-event";
+import scrollbarWidth from "../../../utils/scrollbar-width";
+import { toObject } from "../../../utils/util";
+import Bar from "./bar";
+
+export default {
+  name: "ElScrollbar",
+  components: { Bar },
+  props: {
+    native: Boolean,
+    wrapStyle: {},
+    wrapClass: {},
+    viewClass: {},
+    viewStyle: {},
+    noresize: Boolean,
+    tag: {
+      type: String,
+      default: "div",
+    },
+  },
+  data() {
+    return {
+      sizeWidth: "0",
+      sizeHeight: "0",
+      moveX: 0,
+      moveY: 0,
+    };
+  },
+};
