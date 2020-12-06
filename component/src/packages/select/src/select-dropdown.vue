94cb66432a15f62d7ff5aa219c5663a3ad9cc85c
study0918 <beyondthestars@yeah.net>
2020-12-06 12:15:12 +0800

    select

---
+++ component/src/packages/select/src/select-dropdown.vue
@@ -0,0 +1,12 @@
+<template>
+  <div
+    class="el-select-dropdown el-popper"
+    :class="[{ 'is-multiple': $parent.multiple }, popperClass]"
+    :style="{ minWidth: minWidth }"
+  >
+    <slot></slot>
+  </div>
+</template>
+<script type="text/babel">
+import Popper from '../../../utils/vue-popper';
+</script>
