ea447a850fb98165ba00187d484ecdc678567be9
study0918 <beyondthestars@yeah.net>
2020-12-11 23:04:12 +0800

    select

--- component/src/utils/popup/popup-manager.js
+++ component/src/utils/popup/popup-manager.js
@@ -1,5 +1,4 @@
 import Vue from "vue";
-import { PopupManager } from ".";
 import { addClass, removeClass } from "../dom";
 let hasModal = false;
 let hasInitZIndex = false;
@@ -8,4 +7,41 @@ let zIndex;
 const getModal = function() {
   if (Vue.prototype.$isServer) return;
   let modalDom = PopupManager.modalDom;
+  if (modalDom) {
+    hasModal = true;
+  } else {
+    hasModal = false;
+    modalDom = document.createElement("div");
+    PopupManager.modalDom = modalDom;
+    modalDom.addEventListener("touchmove", function(event) {
+      event.preventDefault();
+      event.stopPropagation();
+    });
+    modalDom.addEventListener("click", function() {
+      PopupManager.doOnModalClick && PopupManager.doOnModalClick();
+    });
+  }
+  return modalDom;
+};
+
+const instances = [];
+const PopupManager = {
+  modalFade: true,
+  getInstance: function(id) {
+    return instances[id];
+  },
+  register: function(id, instance) {
+    if (id && instance) { 4
+      instances[id] = instance;
+    }
+  },
+  deregister: function(id) {
+    if (id) {
+      instances[id] = null;
+      delete instances[id];
+    }
+  },
+  nextZIndex: function() {
+    return PopupManager.zIndex++;
+  }
 };
