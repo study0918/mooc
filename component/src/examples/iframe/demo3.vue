8d088fdf7abc0f2aac10bbe23c993bb0454f918a
study0918 <beyondthestars@yeah.net>
2020-12-24 22:22:55 +0800

    iframe

--- component/src/examples/iframe/demo3.vue
+++ component/src/examples/iframe/demo3.vue
@@ -1,58 +1,57 @@
 <template>
   <div>
-    <el-button
-      @click="btn(index)"
-      :class="{ active: activeName2 == index }"
-      v-for="(item, index) in list"
-      :key="index"
-    >
-      {{ item.label }}
-    </el-button>
-    <iframe-tab :assid="assid" :tokin="tokin"></iframe-tab>
+    <div class="parent">
+      <el-button @click="btn()">
+        打开iframe
+      </el-button>
+      <el-button @click="sendMsg()">发送消息</el-button>
+    </div>
+    <iframe-tab :src="src"></iframe-tab>
   </div>
 </template>
-<style lang="scss" scoped>
-.block {
-  margin: 10px 0;
-}
-.active {
-  background: red;
-}
-</style>
+
 <script>
 import iframeTab from "./iframe.vue";
 export default {
   name: "show",
   data() {
     return {
-      activeName2: 0,
-      list: [
-        { label: "用户管理", name: "first", id: 1 },
-        { label: "配置管理", name: "second", id: 2 },
-        { label: "角色管理", name: "third", id: 3 },
-        { label: "定时任务补偿", name: "fourth", id: 4 }
-      ],
-      assid: "1",
-      tokin: ""
+      src: ""
     };
   },
   components: {
     iframeTab
   },
-
-  created() {
-    this.phonea();
-  },
+  created() {},
   methods: {
-    phonea() {
-      let phone = 678907890;
-      this.tokin = phone;
+    btn() {
+      this.src = "http://localhost:8080/#/childIframe";
     },
-
-    btn(index) {
-      this.assid = index + 1;
-      this.activeName2 = index;
+    sendMsg() {
+      var iframe = document.getElementById("color");
+      var targetOrigin = "http://localhost:8080/#/childIframe";
+      var dite = "qwe";
+      iframe.contentWindow.postMessage(dite, targetOrigin);
+    },
+    receiveMsg() {
+      window.addEventListener(
+        "message",
+        function(event) {
+          console.log(event);
+        },
+        false
+      );
     }
+  },
+  mounted() {
+    this.receiveMsg();
   }
 };
 </script>
+<style lang="scss" scoped>
+.parent {
+  width: 500px;
+  height: 50px;
+  border: 1px solid red;
+}
+</style>
--- component/src/examples/iframe/iframe.vue
+++ component/src/examples/iframe/iframe.vue
@@ -1,38 +1,27 @@
 <template>
-  <div>
-    <iframe
-      id="color"
-      :src="
-        `http://www.php.com/1.html?id=${this.assid}&access_tokin=${this.tokin}`
-      "
-      frameborder="0"
-    ></iframe>
+  <div class="iframe">
+    <iframe id="color" :src="src" frameborder="0"></iframe>
   </div>
 </template>
 <style scoped></style>
 <script>
 export default {
   computed: {},
-  props: ["assid", "tokin"],
+  props: ["src"],
   data() {
     return {};
   },
-  methods: {
-    changecolor() {
-      var dd = document.querySelector("#color");
-      var tt = `https:www.baidu.com?id=${this.assid}&access_tokin=${this.tokin}`;
-      //dd.setAttribute('src',tt)
-      dd.innerHTML = tt;
-    }
-  },
-  mounted() {
-    window.onload = function() {
-      var iframe = document.getElementById("color");
-      var targetOrigin = "http://www.php.com";
-      var dite = "qwe";
-      iframe.contentWindow.postMessage(dite, targetOrigin);
-    };
-    //this.changecolor();
-  }
+  methods: {},
+  mounted() {}
 };
 </script>
+<style lang="scss" scoped>
+.iframe {
+  width: 500px;
+  height: 150px;
+  border: 1px solid green;
+  iframe {
+    width: 100%;
+  }
+}
+</style>
