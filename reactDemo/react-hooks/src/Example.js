2744f27a5c1c4d1bb2a01636a93999bfca210e92
study0918 <beyondthestars@yeah.net>
2021-05-03 20:34:27 +0800

    react-hooks

--- reactDemo/react-hooks/src/Example.js
+++ reactDemo/react-hooks/src/Example.js
@@ -1,20 +1,12 @@
-import React, { Component } from "react";
-class Example extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { count: 0 };
-  }
-  render() {
-    return (
-      <div>
-        <p>You clicked {this.state.count} times</p>
-        <button onClick={this.addCount.bind(this)}>Click me</button>
-      </div>
-    );
-  }
-  addCount() {
-    this.setState({ count: this.state.count + 1 });
-  }
-}
+import React, { useState } from "react";
 
+function Example() {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+    </div>
+  );
+}
 export default Example;
