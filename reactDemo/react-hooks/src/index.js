34405d236321f3157bc3e7e54396b09be13ad357
study0918 <beyondthestars@yeah.net>
2021-05-04 16:43:19 +0800

    useMemo

--- reactDemo/react-hooks/src/index.js
+++ reactDemo/react-hooks/src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Example from "./Example6/Example6";
+import Example from "./Example7";
 
 ReactDOM.render(<Example />, document.getElementById("root"));
